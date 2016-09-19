e32dfba7e9d4cf0c6db802e5ec44ba0348121360
Marcelo dos Santos <mdssjc@gmail.com>
2016-09-19 20:03:47 -0300

    Remoção do código transferido.

--- init.el
+++ init.el
@@ -179,16 +179,7 @@
 ;; ---
 
 ;; Linguagem de Marcação (Markup Language)
-;; Markdown
-(use-package markdown-mode
-  :ensure t
-  :commands (markdown-mode gfm-mode)
-  :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.md\\'"       . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "markdown")
-  :config
-  (setq tab-width 4))
+(require 'markup-language-markdown)
 ;; ---
 
 ;; Automático (Automatic)
