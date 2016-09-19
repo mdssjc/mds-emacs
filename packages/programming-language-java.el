31f35a4d7dd78f81365df2f28b598c34855d18fe
Marcelo dos Santos <mdssjc@gmail.com>
2016-09-19 12:12:47 -0300

    Pacote para a linguagem Java.

---
+++ packages/programming-language-java.el
@@ -0,0 +1,14 @@
+;;
+;; Programming-Language-Java.el
+;;
+;; autor: Marcelo dos Santos
+;; url  : https://github.com/mdssjc/mds-emacs
+;;
+;; Linguagem de Programação (Programming Language)
+;; Java
+;; FIXME -> o pacote está em testes
+(use-package meghanada
+  :ensure t
+  :init (add-hook 'java-mode-hook (lambda () (meghanada-mode t))))
+
+(provide 'programming-language-java)
