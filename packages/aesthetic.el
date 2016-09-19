731501ccc9190007f1ac1ef1351459f43b3eafc7
Marcelo dos Santos <mdssjc@gmail.com>
2016-09-19 00:11:53 -0300

    Movido o código de estética para um novo arquivo.

---
+++ packages/aesthetic.el
@@ -0,0 +1,22 @@
+;;
+;; Aesthetic.el
+;;
+;; autor: Marcelo dos Santos
+;; url  : https://github.com/mdssjc/mds-emacs
+;;
+;; Estético (Aesthetic)
+(use-package spacemacs-theme
+  :ensure t
+  :defer t
+  :init
+  (load-theme 'spacemacs-dark t))
+(use-package spaceline
+  :ensure t
+  :after spacemacs-theme
+  :init (require 'spaceline-config)
+  :config (spaceline-emacs-theme))
+(use-package mode-icons
+  :ensure t
+  :config (mode-icons-mode))
+
+(provide 'aesthetic)
