849a00814aceddce68ea44f4e9240fe7d815e5f7
Marcelo dos Santos <mdssjc@gmail.com>
2016-09-19 20:02:43 -0300

    Código do markdown transferido para o novo arquivo.

---
+++ packages/markup-language-markdown.el
@@ -0,0 +1,19 @@
+;;
+;; Markup-Language-Markdown.el
+;;
+;; autor: Marcelo dos Santos
+;; url  : https://github.com/mdssjc/mds-emacs
+;;
+;; Linguagem de Marcação (Markup Language)
+;; Markdown
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "markdown")
+  :config
+  (setq tab-width 4))
+
+(provide 'markup-language-markdown)
