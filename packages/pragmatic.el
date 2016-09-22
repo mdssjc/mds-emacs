ff92354d15f4d11daeccfef11de62ee73d8669e9
Marcelo dos Santos <mdssjc@gmail.com>
2016-09-21 22:38:27 -0300

    Placeholder para os novos pacotes.

---
+++ packages/pragmatic.el
@@ -0,0 +1,7 @@
+;;
+;; Pragmatic.el
+;;
+;; autor: Marcelo dos Santos
+;; url  : https://github.com/mdssjc/mds-emacs
+;;
+;; Pragmático (Pragmatic)
---
+++ packages/semantic.el
@@ -0,0 +1,7 @@
+;;
+;; Semantic.el
+;;
+;; autor: Marcelo dos Santos
+;; url  : https://github.com/mdssjc/mds-emacs
+;;
+;; Semântico (Semantic)
