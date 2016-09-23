d9968a21ebfb63e64d775da527d5fbc4a1708a58
Marcelo dos Santos <mdssjc@gmail.com>
2016-09-23 14:41:45 -0300

    Draft da configuração do Flyspell.

--- packages/mds-semantic.el
+++ packages/mds-semantic.el
@@ -10,13 +10,14 @@
 ;;; License: Unlicense
 
 ;;; Commentary:
-;; Analisa o código.
+;; Analisa o texto e/ou código.
 
 ;;; Code:
 (use-package flycheck
   :ensure t
   :diminish flycheck-mode " ⓢ"
   :pin melpa
+  :bind (("<f5> s" . flycheck-mode))
   :config
   (use-package flycheck-package :ensure t)
   (use-package flycheck-pos-tip
@@ -25,5 +26,25 @@
   (setq flycheck-check-syntax-automatically '(mode-enable save))
   (eval-after-load 'flycheck '(flycheck-package-setup)))
 
-(provide 'mds-pragmatic)
+;; Em configuração
+(use-package flyspell
+  :ensure t
+  :diminish flyspell-mode " Ⓢ"
+  :bind (("<f5> S" . flyspell-mode))
+  :config
+  (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+  (add-hook 'text-mode-hook 'flyspell-mode)
+  (add-hook 'org-mode-hook 'flycheck-mode)
+  (use-package flyspell-correct-popup
+    :ensure t
+    :commands (flyspell-correct-word-generic
+               flyspell-correct-previous-word-generic)
+    :config
+    (setq flyspell-correct-interface 'flyspell-correct-popup))
+  (use-package flyspell-correct-ivy
+    :ensure t
+    :bind (:map flyspell-mode-map
+                ("C-c $" . flyspell-correct-word-generic))))
+
+(provide 'mds-semantic)
 ;;; mds-semantic.el ends here
