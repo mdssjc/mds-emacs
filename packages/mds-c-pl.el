0222ba691098099209e1f9f1bfaba68ddb4f706b
Marcelo dos Santos <mdssjc@gmail.com>
2016-09-30 23:26:27 -0300

    Placeholder para a linguagem C.

---
+++ packages/mds-c-pl.el
@@ -0,0 +1,18 @@
+;;; mds-c-pl.el --- Linguagem de Programação C (C Programming Language)
+;;
+;; Copyright (C) 2016-2016 Marcelo dos Santos
+;;
+;; author: Marcelo dos Santos <mds>
+;; URL: https://github.com/mdssjc/mds-emacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: Unlicense
+
+;;; Commentary:
+;; Configurações para a linguagem C.
+
+;;; Code:
+
+(provide 'mds-c-pl)
+;;; mds-c-pl ends here
