7c7e0dc4a087b72afbffec9cea874654c0a123db
Marcelo dos Santos <mdssjc@gmail.com>
2016-10-17 20:00:26 -0200

    Inclusão do pacote para a linguagem Haskell.

---
+++ packages/mds-haskell-pl.el
@@ -0,0 +1,18 @@
+;;; mds-haskell-pl.el --- Linguagem de Programação Haskell (Haskell Programming Language)
+;;
+;; Copyright (C) 2016-2016 Marcelo dos Santos
+;;
+;; author: Marcelo dos Santos <mds>
+;; URL: https://github.com/mdssjc/mds-emacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: Unlicense
+
+;;; Commentary:
+;; Configurações para a linguagem Haskell.
+
+;;; Code:
+
+(provide 'mds-haskell-pl)
+;;; mds-haskell-pl ends here
