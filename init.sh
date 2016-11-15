151bb9d4a9e8d3afd270ca9e3bfe320abeb6a855
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-15 21:57:43 -0200

    Script de inicialização.

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# MDS - Script
+#
+# Inicialização dos requisitos do ambiente.
+#
+# autor: Marcelo dos Santos
+#
+git clone https://github.com/rakanalh/emacs-dashboard ~/.emacs.d/temp/dashboard
