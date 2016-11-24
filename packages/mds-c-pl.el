1e8c14e5f716fb9d01b7c1f5cd75a992c81da624
Marcelo dos Santos <mdssjc@gmail.com>
2016-11-24 13:05:37 -0200

    Configuração parcial para a linguagem C.

--- packages/mds-c-pl.el
+++ packages/mds-c-pl.el
@@ -13,6 +13,51 @@
 ;; Configurações para a linguagem C.
 
 ;;; Code:
+(use-package c-mode
+  :ensure nil
+  :mode
+  ("\\.c$" . c-mode)
+  ("\\.h$" . c-mode)
+  :init
+  (setq c-default-style "k&r"
+        speedbar-show-unknow-files t
+        semanticdb-default-save-directory (concat user-emacs-directory ".cache/semanticdb")
+        semantic-default-submodes '(global-semantic-idle-completions-mode
+                                    global-semantic-idle-scheduler-mode
+                                    global-semantic-idle-summary-mode
+                                    global-semanticdb-minor-mode
+                                    global-semantic-stickyfunc-mode))
+  (setq company-backends (remove 'company-clang company-backends))
+  (add-hook 'c-mode-hook '(lambda ()
+                            (set (make-local-variable 'company-transformers)
+                                 '(company-sort-by-backend-importance
+                                   company-sort-prefer-same-case-prefix
+                                   company-sort-by-statistics))
+                            (set (make-local-variable 'company-backends)
+                                 '((company-semantic
+                                    company-c-headers
+                                    company-yasnippet
+                                    :with
+                                    company-keywords
+                                    company-abbrev
+                                    company-dabbrev-code
+                                    company-dabbrev
+                                    company-files)))))
+  (add-hook 'c-mode-hook '(lambda () (semantic-mode t)))
+  (add-hook 'c-mode-hook 'yas-minor-mode)
+  (add-hook 'c-mode-hook 'flycheck-mode)
+  (add-hook 'c-mode-hook (lambda () (setq-local counsel-dash-docsets '("C"))))
+  :config
+  (add-to-list 'company-c-headers-path-system "/usr/include/c++/6.2.1"))
+
+(use-package company-c-headers
+  :ensure t)
+
+(use-package srefactor
+  :ensure t
+  :bind
+  (:map c-mode-map
+   ("<M-return>" . srefactor-refactor-at-point)))
 
 (provide 'mds-c-pl)
 ;;; mds-c-pl ends here
