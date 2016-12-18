d0dbab38bbc2e3d1016655e9d9eee1e7983ff33c
Marcelo dos Santos <mdssjc@gmail.com>
2016-12-18 15:05:30 -0200

    Incremento no pacote Web.

--- packages/mds-web-wl.el
+++ packages/mds-web-wl.el
@@ -16,16 +16,48 @@
 (use-package web-mode
   :ensure t
   :mode
-  ("\\.\\(html\\|htm\\)$" . web-mode)
+  ("\\.html$" . web-mode)
+  ("\\.htm$"  . web-mode)
+  ("\\.css$"  . web-mode)
+  :bind
+  (:map web-mode-map
+        ("<F6> p" . emmet-preview-mode))
+  :init
+  (add-hook 'web-mode-hook 'flycheck-mode)
+  (add-hook 'web-mode-hook '(lambda () (setq-local counsel-dash-docsets '("HTML" "CSS"))))
+  (add-hook 'web-mode-hook '(lambda ()
+                              (set (make-local-variable 'company-transformers)
+                                   '(company-sort-by-backend-importance
+                                     company-sort-prefer-same-case-prefix
+                                     company-sort-by-statistics))
+                              (set (make-local-variable 'company-backends)
+                                   '((company-web
+                                      company-web-html
+                                      company-css
+                                      company-yasnippet
+                                      :with
+                                      company-abbrev
+                                      company-dabbrev-code
+                                      company-dabbrev
+                                      company-files)))))
   :config
-  (setq web-mode-style-padding ers-tab-size
-        web-mode-script-padding ers-tab-size))
+  (setq web-mode-style-padding 2
+        web-mode-script-padding 2
+        web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2
+        web-mode-enable-current-element-highlight t
+        web-mode-enable-current-column-highlight t
+        web-mode-enable-css-colorization t))
 
 (use-package emmet-mode
   :ensure t
   :commands emmet-mode
   :init
-  (add-hook 'html-mode-hook 'emmet-mode))
+  (add-hook 'web-mode-hook 'emmet-mode))
+
+(use-package company-web
+  :ensure t)
 
 (provide 'mds-web-wl)
 ;;; mds-web-wl.el ends here
