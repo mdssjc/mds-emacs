e4a2f82fb63cbf50d714a2dac0cc9ccfb4f86aaa
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-02 21:52:17 -0200

    Refatoração do pacote Org.

--- packages/mds-pragmatic.el
+++ packages/mds-pragmatic.el
@@ -22,10 +22,24 @@
    ("<f9> c" . org-capture)
    ("<f9> b" . org-iswitchb))
   :init
-  (setq org-todo-keywords '((sequence "TODO"
-                                      "DOING"
-                                      "BLOCKED"
-                                      "REVIEW"
+  (add-hook 'org-mode-hook
+            '(lambda () (progn)
+               (setq-local company-minimum-prefix-length 3)
+               (setq-local company-transformers '(company-sort-by-occurrence
+                                                  company-sort-prefer-same-case-prefix
+                                                  company-sort-by-statistics))
+               (setq-local company-backends '((company-capf
+                                               company-abbrev
+                                               company-dabbrev
+                                               company-yasnippet
+                                               :with
+                                               company-ispell)))
+               (company-mode)
+               (flyspell-mode)
+               (org-bullets-mode 1)))
+  :config
+  (require 'ob-java)
+  (setq org-todo-keywords '((sequence "TODO" "DOING" "BLOCKED" "REVIEW"
                                       "|" "DONE" "ARCHIVED" "CANCELED"))
         org-todo-keyword-faces '(("TODO"     . org-warning)
                                  ("DOING"    . "yellow")
@@ -33,31 +47,24 @@
                                  ("REVIEW"   . "orange")
                                  ("DONE"     . "green")
                                  ("ARCHIVED" . "blue")
-                                 ("CANCELED" . "red1")))
-  (add-hook 'org-mode-hook '(lambda ()
-                              (set (make-local-variable 'company-minimum-prefix-length) 3)
-                              (set (make-local-variable 'company-transformers)
-                                   '(company-sort-by-occurrence
-                                     company-sort-prefer-same-case-prefix
-                                     company-sort-by-statistics))
-                              (set (make-local-variable 'company-backends)
-                                   '((company-capf
-                                      company-abbrev
-                                      company-dabbrev
-                                      company-yasnippet
-                                      :with
-                                      company-ispell)))))
-  (add-hook 'org-mode-hook 'company-mode)
-  (add-hook 'org-mode-hook 'flyspell-mode)
-  :config
-  (use-package org-bullets
-    :ensure t
-    :config
-    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-  (require 'ob-java)
+                                 ("CANCELED" . "red1"))
+        org-directory (concat user-emacs-directory "org")
+        org-default-notes-file (concat user-emacs-directory "org/notes.org"))
   (org-babel-do-load-languages 'org-babel-do-load-languages '((emacs-lisp . t)
                                                               (java . t)
                                                               (c . t))))
+
+(use-package org-bullets
+  :ensure t
+  :commands org-bullets-mode)
+
+(use-package org-pomodoro
+  :ensure t
+  :commands org-pomodoro)
+
+(use-package tomatinho
+  :ensure t
+  :commands tomatinho)
 
 (provide 'mds-pragmatic)
 ;;; mds-pragmatic.el ends here
