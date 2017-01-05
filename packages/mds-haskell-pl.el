c782a4066eb68c4a872f78401564c1eabdecdc74
Marcelo dos Santos <mdssjc@gmail.com>
2017-01-05 13:38:01 -0200

    Refatoração no Haskell.

--- packages/mds-haskell-pl.el
+++ packages/mds-haskell-pl.el
@@ -56,53 +56,42 @@
         ("C-c h t"   . hare-refactor-roundtrip)
         ("C-c h s h" . hare-refactor-show))
   :init
-  (use-package haskell-snippets
-    :ensure t
-    :defer t)
-  (setq haskell-process-suggest-remove-import-lines t
-        haskell-process-auto-import-loaded-modules t
-        haskell-process-log t
-        haskell-tags-on-save t
-        haskell-stylish-on-save t
-        haskell-notify-p t
-        haskell-font-lock-symbols t
-        company-ghc-show-info t)
-  (add-hook 'haskell-mode-hook (lambda () (setq-local counsel-dash-docsets '("Haskell"))))
-  (add-hook 'haskell-mode-hook (lambda () (ghc-init) (hare-init)))
-  (add-hook 'haskell-mode-hook 'flyspell-prog-mode)
-  (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-  (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-  (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-  (add-hook 'haskell-mode-hook 'haskell-decl-scan-mode)
-  (add-hook 'haskell-mode-hook 'structured-haskell-mode)
-  (add-hook 'haskell-mode-hook (lambda ()
-                                 (set (make-local-variable 'company-transformers)
-                                      '(company-sort-by-backend-importance
-                                        company-sort-prefer-same-case-prefix
-                                        company-sort-by-statistics))
-                                 (set (make-local-variable 'company-backends)
-                                      '((company-capf
-                                         company-ghc
-                                         company-yasnippet
-                                         :with
-                                         company-abbrev
-                                         company-dabbrev-code
-                                         company-dabbrev
-                                         company-files)))))
-  (add-hook 'haskell-interactive-mode-hook (lambda ()
-                                             (set (make-local-variable 'company-transformers)
-                                                  '(company-sort-by-backend-importance
-                                                    company-sort-prefer-same-case-prefix
-                                                    company-sort-by-statistics))
-                                             (set (make-local-variable 'company-backends)
-                                                  '((company-capf
-                                                     company-ghc
-                                                     company-yasnippet
-                                                     :with
-                                                     company-abbrev
-                                                     company-dabbrev-code
-                                                     company-dabbrev
-                                                     company-files)))))
+  (add-hook 'haskell-mode-hook
+            '(lambda () (progn
+                          (intero-mode)
+                          (intero-mode-whitelist)
+                          (intero-mode-blacklist)
+                          (structured-haskell-mode)
+                          (turn-on-haskell-doc-mode)
+                          (turn-on-haskell-indent)
+                          (haskell-decl-scan-mode)
+                          (rainbow-delimiters-mode)
+                          (speedbar-add-supported-extension ".hs")
+                          (lambda () (ghc-init) (hare-init))
+                          (setq-local company-transformers '(company-sort-by-backend-importance
+                                                             company-sort-prefer-same-case-prefix
+                                                             company-sort-by-statistics))
+                          (setq-local company-backends '((company-intero
+                                                          company-capf
+                                                          company-yasnippet
+                                                          :with
+                                                          company-abbrev
+                                                          company-dabbrev-code
+                                                          company-dabbrev
+                                                          company-files)))
+                          (setq-local counsel-dash-docsets '("Haskell")))))
+  (add-hook 'haskell-interactive-mode-hook
+            '(lambda () (progn
+                          (setq-local company-transformers '(company-sort-by-backend-importance
+                                                             company-sort-prefer-same-case-prefix
+                                                             company-sort-by-statistics))
+                          (setq-local company-backends '((company-capf
+                                                          company-yasnippet
+                                                          :with
+                                                          company-abbrev
+                                                          company-dabbrev-code
+                                                          company-dabbrev
+                                                          company-files))))))
   :config
   ;; GHC
   (add-to-list 'load-path "~/.cabal/share/x86_64-linux-ghc-8.0.1/ghc-mod-5.6.0.0/elisp")
@@ -111,40 +100,42 @@
   ;; HaRe
   (add-to-list 'load-path "~/.cabal/share/x86_64-linux-ghc-8.0.1/HaRe-0.8.3.0/elisp")
   (autoload 'hare-init "hare" nil t)
+  ;; Variables
+  (setq haskell-process-suggest-remove-import-lines t
+        haskell-process-auto-import-loaded-modules t
+        haskell-process-log t
+        haskell-tags-on-save t
+        haskell-stylish-on-save t
+        haskell-notify-p t
+        haskell-font-lock-symbols t
+        company-ghc-show-info t)
   ;; Which-key
-  (mapcar
-   (lambda (mode)
-       (which-key-add-major-mode-key-based-replacements mode
-         "C-c e" "editing"
-         "C-c i" "interpreter"
-         "C-c l" "lookup"
-         "C-c h" "hare"))
-   [haskell-mode literate-haskell-mode]))
+  (mapcar (lambda (mode) (which-key-add-major-mode-key-based-replacements mode
+                      "C-c e" "editing"
+                      "C-c i" "interpreter"
+                      "C-c l" "lookup"
+                      "C-c h" "hare"))
+          [haskell-mode literate-haskell-mode]))
 
-(use-package ghc
+(use-package intero
   :ensure t
-  :commands ghc-init ghc-debug)
-
-(use-package company-ghc
-  :ensure t
-  :commands company-ghc)
+  :commands intero-mode)
 
 (use-package shm
   :ensure t
   :commands structured-haskell-mode)
 
+(use-package haskell-snippets
+  :ensure t
+  :defer t)
+
+(use-package ghc
+  :ensure t
+  :commands ghc-init ghc-debug)
+
 (use-package rainbow-delimiters
   :ensure t
-  :commands rainbow-delimiters-mode
-  :init
-  (add-hook 'haskell-mode-hook 'rainbow-delimiters-mode))
-
-(use-package flycheck-haskell
-  :ensure t
-  :commands flycheck-haskell-setup
-  :init
-  (add-hook 'haskell-mode-hook 'flycheck-haskell-setup)
-  (add-hook 'haskell-mode-hook 'flycheck-mode))
+  :commands rainbow-delimiters-mode)
 
 (provide 'mds-haskell-pl)
 ;;; mds-haskell-pl.el ends here
