6cc7907e4aa6979745695404c91209c85b61a945
Marcelo dos Santos <mdssjc@gmail.com>
2017-02-02 00:18:28 -0200

    Refatoração do pacote Web.

--- packages/mds-web-wl.el
+++ packages/mds-web-wl.el
@@ -23,23 +23,24 @@
   (:map web-mode-map
         ("<F6> p" . emmet-preview-mode))
   :init
-  (add-hook 'web-mode-hook 'flycheck-mode)
-  (add-hook 'web-mode-hook '(lambda () (setq-local counsel-dash-docsets '("HTML" "CSS"))))
-  (add-hook 'web-mode-hook '(lambda ()
-                              (set (make-local-variable 'company-transformers)
-                                   '(company-sort-by-backend-importance
-                                     company-sort-prefer-same-case-prefix
-                                     company-sort-by-statistics))
-                              (set (make-local-variable 'company-backends)
-                                   '((company-web-html
-                                      company-css
-                                      company-yasnippet
-                                      :with
-                                      company-abbrev
-                                      company-dabbrev-code
-                                      company-dabbrev
-                                      company-files)))))
+  (add-hook 'web-mode-hook
+            '(lambda () (progn
+                     (emmet-mode)
+                     (setq-local company-transformers '(company-sort-by-backend-importance
+                                                        company-sort-prefer-same-case-prefix
+                                                        company-sort-by-statistics))
+                     (setq-local company-backends '((company-web-html
+                                                     company-yasnippet
+                                                     :with
+                                                     company-abbrev
+                                                     company-dabbrev-code
+                                                     company-dabbrev
+                                                     company-files)))
+                     (flycheck-mode)
+                     (setq-local counsel-dash-docsets '("HTML" "CSS")))))
   :config
+  (require 'html-mode-expansions)
+  (require 'css-mode-expansions)
   (setq web-mode-style-padding 2
         web-mode-script-padding 2
         web-mode-markup-indent-offset 2
@@ -47,22 +48,23 @@
         web-mode-code-indent-offset 2
         web-mode-enable-current-element-highlight t
         web-mode-enable-current-column-highlight t
-        web-mode-enable-css-colorization t))
+        web-mode-enable-css-colorization t
+        web-mode-enable-auto-expanding t
+        emmet-indentation 2
+        company-minimum-prefix-length 0)
+  (er/add-html-mode-expansions)
+  (er/add-css-mode-expansions))
 
 (use-package emmet-mode
   :ensure t
   :commands emmet-mode
   :bind
   (:map emmet-mode-keymap
-   ("<C-return>" . nil))
-  :init
-  (add-hook 'web-mode-hook 'emmet-mode)
-  :config
-  (setq emmet-indentation 2))
+        ("<C-return>" . nil)))
 
 (use-package company-web
   :ensure t
-  :defer t)
+  :commands company-web-html)
 
 (provide 'mds-web-wl)
 ;;; mds-web-wl.el ends here
