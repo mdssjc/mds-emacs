d8a6db03e110fba8910c428092f878e589fd1747
Marcelo dos Santos <mdssjc@gmail.com>
2017-04-01 13:48:59 -0300

    Configurações do REPL.

--- packages/mds-lisp-pl.el
+++ packages/mds-lisp-pl.el
@@ -164,10 +164,13 @@
   :commands cider-mode
   :init
   (add-hook 'cider-mode-hook 'clj-refactor-mode)
+  (add-hook 'cider-repl-mode-hook 'eldoc-mode)
   :config
   (setq nrepl-log-messages t
         cider-repl-display-in-current-window t
         cider-repl-use-clojure-font-lock t
+        cider-repl-pop-to-buffer-on-connect nil
+        cider-repl-history-file (concat user-emacs-directory ".cache/cider/history-repl")
         cider-prompt-save-file-on-load 'always-save
         cider-font-lock-dynamically '(macro core function var)
         nrepl-hide-special-buffers t
