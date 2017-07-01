406656af9f89c2bd76673298cad91ff10ad2970f
Marcelo dos Santos <mdssjc@gmail.com>
2017-06-30 23:44:26 -0300

    Verificação para criar ou atualizar.

--- init.sh
+++ init.sh
@@ -5,5 +5,14 @@
 #
 # autor: Marcelo dos Santos
 #
-mkdir git
-git clone https://github.com/emacs-ess/ESS.git ~/.emacs.d/git/ESS
+if [ ! -d ./git ]; then
+    mkdir git
+fi
+
+if [ ! -d ./git/ESS ]; then
+    git clone https://github.com/emacs-ess/ESS.git ~/.emacs.d/git/ESS
+else
+    cd ./git/ESS
+    git pull
+    cd ../../
+fi
