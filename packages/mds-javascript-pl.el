d984124005d18831c406926028e476c6b7de7f46
Marcelo dos Santos <mdssjc@gmail.com>
2017-10-27 20:03:05 -0200

    Melhorias no JS (em testes).

--- packages/mds-javascript-pl.el
+++ packages/mds-javascript-pl.el
@@ -21,25 +21,22 @@
   ("node" . j2-mode)
   :init
   (add-hook 'js2-mode-hook
-            '(lambda ()
-               (js2-imenu-extras-mode)
-               (js2-refactor-mode)
-               ;;(tern-mode t)
-               (lsp-mode)
-               (js-auto-beautify-mode)
-               (electric-pair-mode)
-               (show-paren-mode)
-               (add-hook 'xref-backend-functions 'xref-js2-xref-backend nil t)
-               (setq-local company-transformers '(company-sort-prefer-same-case-prefix))
-               (setq-local company-minimum-prefix-length 1)
-               (setq-local company-idle-delay 0)
-               (setq-local company-backends '((
-                                               ;;company-tern
-                                               company-lsp
-                                               company-abbrev
-                                               company-dabbrev-code
-                                               company-dabbrev
-                                               company-files)))))
+            (lambda ()
+              (lsp-javascript-typescript-enable)
+              (js2-imenu-extras-mode)
+              (js2-refactor-mode)
+              (js-auto-beautify-mode)
+              (electric-pair-mode)
+              (show-paren-mode)
+              (add-hook 'xref-backend-functions 'xref-js2-xref-backend nil t)
+              (setq-local company-transformers '(company-sort-prefer-same-case-prefix))
+              (setq-local company-minimum-prefix-length 1)
+              (setq-local company-idle-delay 0)
+              (setq-local company-backends '((company-lsp
+                                              company-abbrev
+                                              company-dabbrev-code
+                                              company-dabbrev
+                                              company-files)))))
   :config
   (setq js2-highlight-level 3
         js-indent-level 2))
@@ -54,25 +51,13 @@
   :ensure t
   :after js2-mode)
 
-(use-package tern
-  :ensure t
-  :commands tern-mode
-  :config
-  (setq tern-command (append tern-command '("--no-port-file"))))
-
-(use-package company-tern
-  :ensure t
-  :commands company-tern
-  :config
-  (setq company-tern-property-marker nil))
-
 (use-package js-auto-beautify
   :ensure t
   :commands js-auto-beautify-mode)
 
 (use-package lsp-javascript-typescript
   :ensure t
-  :after js2-mode)
+  :commands lsp-javascript-typescript-enable)
 
 (provide 'mds-javascript-pl)
 ;;; mds-javascript-pl.el ends here
