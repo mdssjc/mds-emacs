4ac2c162ce3eb67ffb98c1f2a71547a9513eaad0
Marcelo dos Santos <mdssjc@gmail.com>
2017-12-03 19:55:44 -0200

    Nova extensão para o use-package.

--- packages/mds-structure.el
+++ packages/mds-structure.el
@@ -371,6 +371,9 @@
   :ensure t
   :commands package-utils-install-async package-utils-upgrade-all)
 
+(use-package use-package-ensure-system-package
+  :ensure t)
+
 (use-package exec-path-from-shell
   :ensure t
   :commands exec-path-from-shell-initialize
