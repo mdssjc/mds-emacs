6a750a076095911539ec89f2f8fdfa98f35a0792
Marcelo dos Santos <mdssjc@gmail.com>
2017-12-27 19:38:40 -0200

    Dica do Abo-abo em http://oremacs.com/2017/12/27/company-numbers.

--- packages/mds-syntax.el
+++ packages/mds-syntax.el
@@ -72,7 +72,30 @@
         company-dabbrev-minimum-length 3
         company-dabbrev-ignore-case nil
         company-dabbrev-downcase nil
-        company-dabbrev-ignore-invisible t))
+        company-dabbrev-ignore-invisible t)
+  ;; http://oremacs.com/2017/12/27/company-numbers/
+  (defun ora-company-number ()
+    "Forward to `company-complete-number'.
+
+     Unless the number is potentially part of the candidate.
+     In that case, insert the number."
+    (interactive)
+    (let* ((k (this-command-keys))
+           (re (concat "^" company-prefix k)))
+      (if (cl-find-if (lambda (s) (string-match re s))
+                      company-candidates)
+          (self-insert-command 1)
+        (company-complete-number (string-to-number k)))))
+  (let ((map company-active-map))
+    (mapc
+     (lambda (x)
+       (define-key map (format "%d" x) 'ora-company-number))
+     (number-sequence 0 9))
+    (define-key map " " (lambda ()
+                        (interactive)
+                        (company-abort)
+                        (self-insert-command 1)))
+    (define-key map (kbd "<return>") nil)))
 
 (use-package company-quickhelp
   :ensure t
